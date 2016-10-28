31a9966c6a987f81b0a2836410728f55726dfa0d
denny7794 <denny7794@gmail.com>
2016-10-28 07:42:37 +0400

    пустышки

--- src/GameSpaceInvaders.java
+++ src/GameSpaceInvaders.java
@@ -1,5 +1,45 @@
+import javax.swing.*;
+import java.awt.*;
+
 /**
+ * Classic game of Space Invaders
+ *
  * Created by Denis on 28.10.2016.
  */
-public class GameSpaceInvaders {
+public class GameSpaceInvaders extends JFrame{
+
+    public static void main(String[] args) {
+        new GameSpaceInvaders().go();
+    }
+
+    GameSpaceInvaders() {
+
+    }
+
+    void go() {
+
+    }
+
+    class Cannon {
+
+    }
+
+    class Ray {
+
+    }
+
+    class Alien {
+
+    }
+
+    class Wave {
+
+    }
+
+    class Canvas extends JPanel {
+        @Override
+        public void paint(Graphics g) {
+            super.paint(g);
+        }
+    }
 }
